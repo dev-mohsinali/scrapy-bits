0c5e42fe24305bee48d4e778ef73017ae064dd04
Mohsin Ali <dev.mohsinali@gmail.com>
2022-03-20 11:43:06 +0500

    Cleaning and transforming returned items using items pipeline and item loaders

--- amazon_review_with_itemloaders/amazon_review_with_itemloaders/items.py
+++ amazon_review_with_itemloaders/amazon_review_with_itemloaders/items.py
@@ -1,12 +1,39 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from itemloaders.processors import (TakeFirst,MapCompose)
+from w3lib.html import remove_tags
+from dateutil import parser
+import re
 
+def get_rating(value):
+    return value[:3]
+def get_date(value):
+    date_string = re.findall(r"on(.*)", value)
+    return parser.parse(''.join(map(str, date_string)).strip())
+def get_country_region(value):
+    country_region = re.findall(r"the(.*)on", value)
+    return ''.join(map(str, country_region)).strip()
 
-class AmazonReviewWithItemloadersItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ReviewItem(scrapy.Item):
+    rating = scrapy.Field(
+        input_processor=MapCompose(get_rating),
+        output_processor=TakeFirst()
+    )
+    title = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    date = scrapy.Field(
+         input_processor=MapCompose(get_date),
+        output_processor=TakeFirst()
+    )
+    country_region = scrapy.Field(
+         input_processor=MapCompose(get_country_region),
+        output_processor=TakeFirst()
+    )
+    
+    review = scrapy.Field(
+        input_processor=MapCompose(remove_tags, str.strip),
+        output_processor=TakeFirst()
+    )
+    page_url = scrapy.Field(
+        output_processor=TakeFirst()
+    )
